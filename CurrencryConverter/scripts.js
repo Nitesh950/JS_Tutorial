f5a4069fec471eee08148fe2bb86d291f4c212eb
Nitesh Kumar Mishra <nkmishra342@gmail.com>
2024-01-14 01:01:17 +0530

    Currency converted

--- CurrencryConverter/scripts.js
+++ CurrencryConverter/scripts.js
@@ -1,75 +1,66 @@
 const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-let button = document.querySelector(".btn");
-
-window.addEventListener("load", convert);
-
-
-let dropdowns = document.querySelectorAll(".country-selector select");
+let dropdowns = document.querySelectorAll(".country-box select");
 
 for(let select of dropdowns){
     for(let codes in countryList){
         let newOption = document.createElement("option");
-        newOption.innerText = codes;
-        newOption.value = codes;
+        newOption.innerText=codes;
+        newOption.value=codes;
+        select.append(newOption);
+
         if(select.name==="from" && codes==="USD"){
             newOption.selected="selected";
         }
+
         if(select.name==="to" && codes==="INR"){
             newOption.selected="selected";
         }
-        select.append(newOption);
     }
-    select.addEventListener("change", (event) => {
-        updateFlag(event.target);
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
     })
+
 }
 
 function updateFlag(element){
     let currCode = element.value;
-    let image = element.parentElement.querySelector("img"); 
-    let countryCode = countryList[currCode];
-    let newImage = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    image.src=newImage;
+    let country = countryList[currCode];
+    let flagURL = `https://flagsapi.com/${country}/flat/64.png`;
+
+    let image = element.parentElement.querySelector("img");
+    image.src=flagURL;
 }
 
-let fromCurrency = document.querySelector(".from-country select");
-let toCurrency = document.querySelector(".to-country select");
 
+let button = document.querySelector(".convert-button");
 
 button.addEventListener("click", (event) => {
     event.preventDefault();
     convert();
+})
 
-});
+let fromCurrency = document.querySelector(".from-country select");
+let toCurrency = document.querySelector(".to-country select");
 
-
-
+let resultMessage = "";
 async function convert(){
-    let amountInput = document.querySelector(".amount input");
-    let enteredAmount = amountInput.value;
-
-    let url = `${BASE_URL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
-
-
-    let response =  await fetch(url);
+    // /usd/inr.json
+    let fromCurrencyValue = fromCurrency.value.toLowerCase();
+    let toCurrencyValue = toCurrency.value.toLowerCase();
+    let url = `${BASE_URL}/${fromCurrencyValue}/${toCurrencyValue}.json`;
+    let response = await fetch(url);
     let data = await response.json();
-   
-    let conversionFactor = data[toCurrency.value.toLowerCase()];
-
-    let finalValue= enteredAmount * conversionFactor;
- 
-
-    let exchangeResult = document.querySelector("#exchange-msg");
-    exchangeResult.innerText = `${enteredAmount} ${fromCurrency.value} = ${finalValue} ${toCurrency.value}`;
-
     
+    let conversionFactor = data[toCurrencyValue];
+    let initialAmount = document.querySelector(".amount-value");
+    if(initialAmount.value==="" || initialAmount.value <= 0){
+        resultMessage = "Please enter a valid amount";
+    }else{
+       let finalValue = initialAmount.value * conversionFactor;
+       resultMessage = `${initialAmount.value} ${fromCurrency.value} = ${finalValue} ${toCurrency.value}`;
+    }
+
+    let conversionMessage = document.querySelector(".conversion-message");
+    conversionMessage.innerText = resultMessage;
 }
-
-
-
-
-
-
-
-
